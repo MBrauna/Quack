74d5aa7f70924606a35499e864727e21c7e98762
MBrauna <michelbrauna@gmail.com>
2019-03-09 17:15:01 -0300

    Quack Server
    Aplicação Quack Server

---
+++ QuackServer/Database/postgresql.sql
@@ -0,0 +1,234 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+----------------------------------------------------------------------------
+----------------------------------------------------------------------------
+--                          TABELAS PARA MBServer                         --
+----------------------------------------------------------------------------
+----------------------------------------------------------------------------
+create sequence seq_cliente increment 1 start 1 minvalue 1 maxvalue 999999999;
+CREATE TABLE cliente
+(
+   id_cliente       integer                     NOT NULL
+  ,nome             text                        NOT NULL
+  ,nome_fantasia    text
+  ,cnpj_cpf         integer
+  ,tipo_pessoa      integer                     NOT NULL DEFAULT 0
+  ,data_cadastro    timestamp without time zone NOT NULL DEFAULT now()
+  ,vip              integer                     NOT NULL DEFAULT 0
+  ,admin            integer                     NOT NULL DEFAULT 0
+  ,CONSTRAINT pk_cliente            PRIMARY KEY (id_cliente)
+  ,CONSTRAINT uk_cliente_cnpjcpf    UNIQUE (cnpj_cpf)
+  ,CONSTRAINT ck_cliente_vip        CHECK (vip in (0,1))
+  ,CONSTRAINT ck_cliente_tppessoa   CHECK (tipo_pessoa in (0,1))
+  ,CONSTRAINT ck_cliente_admin      CHECK (admin in (0,1))
+);
+
+
+comment on column cliente.id_cliente     is 'Código único - SEQUENCIAL DO CLIENTE';
+comment on column cliente.nome           is 'Nome do cliente - Se pessoa jurídica nome real.';
+comment on column cliente.nome_fantasia  is 'Nome fantasia ou apelido do cliente';
+comment on column cliente.cnpj_cpf       is 'Código de cadastro à receita federal';
+comment on column cliente.tipo_pessoa    is '[0] - Pessoa física, [1] - Pessoa Jurídica';
+comment on column cliente.vip            is '[0] - Não, [1] - Sim';
+comment on column cliente.admin          is '[0] - Não, [1] - Sim';
+comment on table  cliente                is 'Tabela de clientes - Toda pessoa física ou jurídica que por algum motivo iteragir com a aplicação deverá ser cadastrada nesta tabela';
+
+
+create index idx_cliente_nome          on cliente(nome asc nulls last);
+create index idx_cliente_cnpj_cpf      on cliente(cnpj_cpf asc nulls last);
+create index idx_cliente_tpessoa       on cliente(tipo_pessoa asc nulls last);
+create index idx_cliente_dtcadastro    on cliente(data_cadastro asc nulls last);
+create index idx_cliente_admin         on cliente(admin asc nulls last);
+create index idx_cliente_vip           on cliente(vip asc nulls last);
+
+CREATE FUNCTION public.tg_ins_cliente() RETURNS trigger AS
+$BODY$
+declare
+begin
+  if (TG_OP = 'INSERT') then
+    new.id_cliente    := nextval('seq_cliente');
+    new.data_cadastro := now();
+
+    if new.tipo_pessoa is null then
+      if length(new.cnpj_cpf) <= 11 then
+        new.tipo_pessoa := 0; -- [0] - Pessoa física
+      else
+        new.tipo_pessoa := 1; -- [1] - Pessoa Jurídica
+      end if; -- if length(new.cnpj_cpf) <= 11 then
+    end if; -- if new.tipo_pessoa is null then
+    
+  elsif (TG_OP = 'UPDATE') then
+    new.id_cliente    := old.id_cliente;
+    new.data_cadastro := old.data_cadastro;
+  elsif (TG_OP = 'DELETE') then
+    RAISE INFO 'Não é possível remover o cliente %', now();
+  end if;
+
+  return new;
+end;$BODY$
+LANGUAGE plpgsql VOLATILE NOT LEAKPROOF;
+
+CREATE TRIGGER tg_biur_cliente BEFORE INSERT OR UPDATE OR DELETE
+   ON public.cliente FOR EACH ROW
+   EXECUTE PROCEDURE public.tg_ins_cliente();
+
+
+
+
+
+
+
+
+----------------------------------------------------------------------------
+----------------------------------------------------------------------------
+create sequence seq_sistema increment 1 start 1 minvalue 1 maxvalue 999999999;
+CREATE TABLE sistema
+(
+   id_sistema           integer     NOT NULL
+   hash                 text        NOT NULL
+   ativo                integer     NOT NULL DEFAULT 0
+   descricao            text        NOT NULL
+   CONSTRAINT pk_sistema PRIMARY KEY (id_sistema)
+   CONSTRAINT uk_sistema_hash UNIQUE (hash)
+   CONSTRAINT ck_sistema_ativo CHECK (ativo in (0,1))
+);
+
+
+comment on column sistema.id_sistema    is 'Código sequencial - SISTEMAS DISPONÍVEIS';
+comment on column sistema.hash          is 'Código único para uso do sistema';
+comment on column sistema.ativo         is '[0] - Inativo, [1] - Ativo';
+comment on column sistema.descricao     is 'Descrição do sistema vigente';
+comment on table  sistema               is 'Sistemas MBrauna disponíveis para uso';
+
+create index idx_sistema_hash   on sistema(hash asc nulls last);
+create index idx_sistema_ativo  on sistema(ativo asc nulls last);
+
+
+CREATE FUNCTION public.tg_sistema() RETURNS trigger AS
+$BODY$begin
+  if (TG_OP = 'INSERT') then
+    new.id_sistema := nextval('seq_sistema');
+    new.hash       := md5(to_char(now(),'DDMMYYYYHH24MISS') || new.id_sistema);
+  elsif (TG_OP = 'UPDATE') then
+    new.id_sistema := old.id_sistema; -- Impede alteração do ID
+    new.hash       := old.hash;
+  else
+    RAISE INFO 'Não é possível remover o sistema %', now();
+  end if;
+
+  return new;
+end;$BODY$
+LANGUAGE plpgsql VOLATILE NOT LEAKPROOF;
+
+
+CREATE TRIGGER tg_biur_sistema BEFORE INSERT OR UPDATE OR DELETE
+   ON public.sistema FOR EACH ROW
+   EXECUTE PROCEDURE public.tg_sistema();
+
+
+
+
+
+
+
+
+----------------------------------------------------------------------------
+----------------------------------------------------------------------------
+create sequence seq_elemento increment 1 start 1 minvalue 1 maxvalue 999999999;
+create table elemento
+(
+    id_elemento         integer     not null    default nextval('seq_elemento')
+   ,descricao           text        not null    default 'ELEMENTO INVÁLIDO'
+   ,item_nome           text        not null    default '00000000'
+   ,ativo               integer     not null    default 0
+   ,primary key(id_elemento)
+   ,constraint ck_elemento_ativo        check (ativo in (0,1))
+   ,constraint uk_elemento_itemnome     unique (item_nome)
+);
+comment on column elemento.id_elemento  is 'Código único para referência do elemento';
+comment on column elemento.descricao    is 'Descrição visual do elemento';
+comment on column elemento.item_nome    is 'Referência do item em MBraunaCore';
+comment on column elemento.ativo        is '[0] - Inativo, [1] - Ativo';
+
+create index idx_elemento_desc          on elemento(descricao asc nulls last);
+create index idx_elemento_itemnome      on elemento(item_nome asc nulls last);
+create index idx_elemento_ativo         on elemento(ativo asc nulls last);
+
+
+
+
+
+
+
+
+----------------------------------------------------------------------------
+----------------------------------------------------------------------------
+create sequence seq_sistema_acesso increment 1 start 1 minvalue 1 maxvalue 999999999;
+create table public.sistema_acesso
+(
+  ,id_sistema_acesso        integer     not null
+  ,chave_acesso             text        not null
+  ,id_cliente               integer     not null
+  ,id_sistema               integer     not null
+  ,situacao                 integer     not null default 0
+  constraint pk_sistema_acesso              primary key (id_sistema_acesso),
+  constraint fk_sistema_acesso_idcliente    foreign key (id_cliente) references public.cliente (id_cliente) match simple on update no action on delete no action,
+  constraint fk_sistema_acesso_idsistema    foreign key (id_sistema) references public.sistema (id_sistema) match simple on update no action on delete no action,
+  constraint ck_sistema_acesso_situacao     check (situacao = any (array[0, 1]))
+);
+
+comment on table  sistema_acesso                     is 'Tabela de liberação de acessos por período';
+comment on column sistema_acesso.id_sistema_acesso   is 'Código sequencial - Dado único para acesso ao sistema';
+comment on column sistema_acesso.chave_acesso        is 'Chave de acesso ao sistema - Através desta chave os dados serão atualizados em MBCore';
+comment on column sistema_acesso.id_cliente          is 'Chave de referência aos dados do cliente';
+comment on column sistema_acesso.id_sistema          is 'Código de referência ao sistema';
+comment on column sistema_acesso.situacao            is '[0] - Inativo, [1] - Ativo';
+
+CREATE FUNCTION public.tg_sistema_acesso() RETURNS trigger AS
+$BODY$declare
+  v_contador integer;
+begin
+  select count(1)
+    into v_contador
+    from sistema_acesso sa
+   where sa.id_cliente   = new.id_cliente
+     and sa.id_sistema   = new.id_sistema
+     and sa.sistuacao    = 1
+        ;
+
+  if v_contador > 0 then
+    raise info 'Já existe um registro definido para o cliente %! Verifique.', new.id_cliente;
+  end if;
+
+  if (TG_OP = 'INSERT') then
+    new.id_sistema_acesso := nextval('seq_sistema_acesso');
+    new.chave_acesso      := md5(to_char(now(),'DDMMYYYYHH24MISS') || new.id_sistema_acesso);
+  elsif (TG_OP = 'UPDATE') then
+    new.id_sistema_acesso := old.id_sistema_acesso;
+    new.chave_acesso      := old.chave_acesso;
+    new.id_cliente        := old.id_cliente;
+    new.id_sistema        := old.id_sistema;
+  else
+    raise info 'Não é possível remover o registro SISTEMA_ACESSO %', now();
+  end if;
+end;$BODY$
+LANGUAGE plpgsql VOLATILE NOT LEAKPROOF;
+
+CREATE TRIGGER tg_biur_sistema_acesso BEFORE INSERT OR UPDATE OR DELETE
+   ON public.sistema_acesso FOR EACH ROW
+   EXECUTE PROCEDURE public.tg_sistema_acesso();
---
+++ QuackServer/quackserver.py
@@ -0,0 +1,283 @@
+'''
+Autor: Michel Brauna                             Data: 12/01/2011
+
+         ██████╗ ██╗   ██╗ █████╗  ██████╗██╗  ██╗
+        ██╔═══██╗██║   ██║██╔══██╗██╔════╝██║ ██╔╝
+        ██║   ██║██║   ██║███████║██║     █████╔╝ 
+        ██║▄▄ ██║██║   ██║██╔══██║██║     ██╔═██╗ 
+        ╚██████╔╝╚██████╔╝██║  ██║╚██████╗██║  ██╗
+         ╚══▀▀═╝  ╚═════╝ ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝
+'''
+
+# Carrega as bibliotecas para MBrauna Core
+import  config
+import  os
+import  signal
+import  sys
+# Carrega as bibliotecas para MBrauna Core
+
+
+
+class quackserver:
+    '''
+    Procedimento para composição do núcleo Quack Server
+
+         ██████╗ ██╗   ██╗ █████╗  ██████╗██╗  ██╗       
+        ██╔═══██╗██║   ██║██╔══██╗██╔════╝██║ ██╔╝       
+        ██║   ██║██║   ██║███████║██║     █████╔╝        
+        ██║▄▄ ██║██║   ██║██╔══██║██║     ██╔═██╗        
+        ╚██████╔╝╚██████╔╝██║  ██║╚██████╗██║  ██╗       
+         ╚══▀▀═╝  ╚═════╝ ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝       
+                                                         
+        ███████╗███████╗██████╗ ██╗   ██╗███████╗██████╗ 
+        ██╔════╝██╔════╝██╔══██╗██║   ██║██╔════╝██╔══██╗
+        ███████╗█████╗  ██████╔╝██║   ██║█████╗  ██████╔╝
+        ╚════██║██╔══╝  ██╔══██╗╚██╗ ██╔╝██╔══╝  ██╔══██╗
+        ███████║███████╗██║  ██║ ╚████╔╝ ███████╗██║  ██║
+        ╚══════╝╚══════╝╚═╝  ╚═╝  ╚═══╝  ╚══════╝╚═╝  ╚═╝
+
+    '''
+
+    MB_BANCO_DADOS                  =   {
+                                            'MB_TIPO'           :   0
+                                           ,'MB_HOSPEDEIRO'     :   '127.0.0.1'
+                                           ,'MB_PORTA'          :   0
+                                           ,'MB_USUARIO'        :   'xyz'
+                                           ,'MB_SENHA'          :   'xyz'
+                                           ,'MB_BANCO'          :   'xyz'
+                                        }
+
+    MB_VISAO                        =   {
+                                            'MB_INSTANCIA'      :   None
+                                           ,'MB_CAMERA'         :   [['WebCam',0]]
+                                        }
+
+    MB_CONFIGURACAO                 =   {
+                                            'MB_CHAVE'          :   '123456'
+                                           ,'MB_RASTREIO_ERRO'  :   5
+                                           ,'MB_DIRETORIO_LOG'  :   '/var/log'
+                                        }
+    # Inicialização MBrauna Core -> Procedimento de inicialização
+    def __init__(self):
+        try:
+            # ---------------------------------------------------------------- #
+            #  CARREGA AS CONFIGURAÇÕES DO ARQUIVO CONFIG.PY PARA O PROCESSO.  #
+            # ---------------------------------------------------------------- #
+            # ---------------------------------------------------------------- #
+            #      BANCO DE DADOS E SUAS RESPECTIVAS CAMADAS DE EXECUÇÃO       #
+            # ---------------------------------------------------------------- #
+            # Teste para o tipo de banco de dados
+            if self.validaNumerico(config.MB_DB_TIPO) and config.MB_DB_TIPO in (0,1,2,3,4,5):
+                self.MB_BANCO_DADOS['MB_TIPO']              =   config.MB_DB_TIPO
+            # Teste para o tipo de banco de dados
+
+            # Teste para o endereço do hospedeiro do banco de dados
+            if self.validaString(config.MB_DB_HOSPEDEIRO):
+                self.MB_BANCO_DADOS['MB_HOSPEDEIRO']        =   config.MB_DB_HOSPEDEIRO
+            # Teste para o endereço do hospedeiro do banco de dados
+
+            # Teste para a porta do servidor do banco de dados
+            if self.validaNumerico(config.MB_DB_PORTA):
+                self.MB_BANCO_DADOS['MB_PORTA']             =   config.MB_DB_PORTA
+            # Teste para a porta do servidor do banco de dados
+
+            # Teste para usuário do banco de dados
+            if self.validaString(config.MB_DB_USUARIO):
+                self.MB_BANCO_DADOS['MB_USUARIO']           =   config.MB_DB_USUARIO
+            # Teste para usuário do banco de dados
+
+            # Teste para senha do banco de dados
+            if self.validaString(config.MB_DB_SENHA):
+                self.MB_BANCO_DADOS['MB_SENHA']             =   config.MB_DB_SENHA
+            # Teste para senha do banco de dados
+
+            # Teste para nome do banco de dados
+            if self.validaString(config.MB_DB_BANCO):
+                self.MB_BANCO_DADOS['MB_BANCO']             =   config.MB_DB_BANCO
+            # Teste para nome do banco de dados
+            # ---------------------------------------------------------------- #
+            #      BANCO DE DADOS E SUAS RESPECTIVAS CAMADAS DE EXECUÇÃO       #
+            # ---------------------------------------------------------------- #
+
+            # Inicia a comunicação com o banco de dados
+            self.MB_BANCO_DADOS['MB_CONEXAO']               =   conexao(self.MB_BANCO_DADOS)
+            # Inicia a comunicação com o banco de dados
+
+            # Inicia o parametro de chave de usuário
+            if self.validaString(config.MB_CHAVE_ACESSO):
+                self.MB_CONFIGURACAO['MB_CHAVE']            =   config.MB_CHAVE_ACESSO
+
+            if self.validaNumerico(config.MB_RASTREIO_ERRO):
+                self.MB_CONFIGURACAO['MB_RASTREIO_ERRO']    =   config.MB_RASTREIO_ERRO
+            # INicia o parametro de chave de usuário
+
+            # Parâmetro para salvar log
+            if self.validaString(config.MB_DIRETORIO_LOG):
+                self.MB_CONFIGURACAO['MB_DIRETORIO_LOG']    =   config.MB_DIRETORIO_LOG
+            # ---------------------------------------------------------------- #
+            #  CARREGA AS CONFIGURAÇÕES DO ARQUIVO CONFIG.PY PARA O PROCESSO.  #
+            # ---------------------------------------------------------------- #
+
+            self.exec_visao()
+
+        except Exception as p_erro:
+            # Encerra a conexão com o DB
+            self.MB_BANCO_DADOS['MB_CONEXAO'].encerra_DB()
+            # Encerra a conexão com o DB
+            # Mais detalhes sobre o erro
+            ecx_tipo, ecx_obj, ecx_dados    =   sys.exc_info()
+            ecx_nome                        =   os.path.split(ecx_dados.tb_frame.f_code.co_filename)[1]
+
+            vtmp_erro   =   '[CORE][INIT][ERRO][' + ecx_dados.tb_lineno + '] - ' + p_erro
+            self.proc_salva_arquivo(vtmp_erro)
+        # Inicialização MBrauna Core -> Procedimento de inicialização
+
+
+    # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- #
+
+
+    # Função para captura do sinal de parada
+    def __exit__(self, exc_type, exc_value, traceback):
+        # Mais detalhes sobre o erro
+        ecx_tipo, ecx_obj, ecx_dados    =   sys.exc_info()
+        ecx_nome                        =   os.path.split(ecx_dados.tb_frame.f_code.co_filename)[1]
+
+        vtmp_erro   =   '[CORE][EXIT][MSG][' + ecx_dados.tb_lineno + '] - ' + p_erro
+        self.proc_salva_arquivo(vtmp_erro)
+        self.MB_BANCO_DADOS['MB_CONEXAO'].encerra_DB()
+    # Função para captura do sinal de parada
+    # ---------------------------------------------------------------- #
+    #                 PROCEDIMENTOS DE SUPORTE AO CORE                 #
+    # ---------------------------------------------------------------- #
+
+    # Função para validação de strings, se foram preenchidas ou não
+    def validaString(self, p_string):
+        try:
+            if p_string and p_string is not None and type(p_string) == str and p_string.strip():
+                return True
+            
+            return False
+        except Exception as p_erro:
+            # Mais detalhes sobre o erro
+            ecx_tipo, ecx_obj, ecx_dados    =   sys.exc_info()
+            ecx_nome                        =   os.path.split(ecx_dados.tb_frame.f_code.co_filename)[1]
+
+            vtmp_erro   =   '[CORE][VALIDASTRING][ERRO][' + ecx_dados.tb_lineno + '] - ' + p_erro
+            self.proc_salva_arquivo(vtmp_erro)
+
+            return False
+    # Função para validação de strings, se foram preenchidas ou não
+
+
+    # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- #
+
+
+    # Função para validação de numericos, se foram preenchidas ou não
+    def validaNumerico(self, p_numerico):
+        try:
+            if p_numerico and p_numerico is not None and type(p_numerico) in (type(1), type(.1)):
+                return True
+
+            return False
+        except Exception as p_erro:
+            # Mais detalhes sobre o erro
+            ecx_tipo, ecx_obj, ecx_dados    =   sys.exc_info()
+            ecx_nome                        =   os.path.split(ecx_dados.tb_frame.f_code.co_filename)[1]
+
+            vtmp_erro   =   '[CORE][VALIDANUMERICO][ERRO][' + ecx_dados.tb_lineno + '] - ' + p_erro
+            self.proc_salva_arquivo(vtmp_erro)
+            return False
+    # Função para validação de numericos, se foram preenchidas ou não
+
+    # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- #
+
+    # Procedure para salvar log dos dados de saída para o arquivo
+    def proc_salva_arquivo(self, p_texto):
+        try:
+            v_caminho       =   self.MB_CONFIGURACAO['MB_DIRETORIO_LOG'] + "MBLOG" + strftime("%Y%m%d%H%M%S", gmtime()) +".log"
+            vtmp_arquivo    =   open(v_caminho,"a+")
+            vtmp_arquivo.write(p_texto)
+            vtmp_arquivo.close()
+        except Exception as p_erro:
+            # Mais detalhes sobre o erro
+            ecx_tipo, ecx_obj, ecx_dados    =   sys.exc_info()
+            ecx_nome                        =   os.path.split(ecx_dados.tb_frame.f_code.co_filename)[1]
+            # Mais detalhes sobre o erro
+            print('[ALERTA] - Ocorreu um erro ao salvar o log - [ALERTA]\n',p_erro)
+            print(ecx_tipo, '\n', ecx_obj, '\n', ecx_dados, '\n', ecx_nome,'\nLinha[',ecx_dados.tb_lineno,']')
+    # Procedure para salvar log dos dados de saída para o arquivo
+
+    # ---------------------------------------------------------------- #
+    #                 PROCEDIMENTOS DE SUPORTE AO CORE                 #
+    # ---------------------------------------------------------------- #
+
+    # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- #
+
+    # ---------------------------------------------------------------- #
+    #                 PROCEDIMENTOS PRINCIPAIS AO CORE                 #
+    # ---------------------------------------------------------------- #
+    # Coleta de dados da câmera e execução dos procedimentos necessários
+    def lista_cameras(self):
+        try:
+            # Coleta todas as câmeras para execução do pré-processamento de dados
+            vtmp_erro   =   '[CORE][LISTA_CAMERAS][MSG] - Inicialização das câmeras token ' + str(self.MB_CONFIGURACAO['MB_CHAVE'])
+            self.proc_salva_arquivo(vtmp_erro)
+
+            return self.MB_BANCO_DADOS['MB_CONEXAO'].executa_consulta("select cs.descricao as descricao, cs.url_camera as url, cs.ponto_corte as ponto_corte, cs.id_sistema_camera as id_sistema_camera from sistema s inner join sistema_camera cs on cs.hash_sistema = s.hash where s.hash = '" + str(self.MB_CONFIGURACAO['MB_CHAVE']) + "' and s.ativo = 1 and cs.ativo = 1 order by cs.descricao")
+            # Coleta todas as câmeras para execução do pré-processamento de dados
+        except Exception as p_erro:
+            # Mais detalhes sobre o erro
+            ecx_tipo, ecx_obj, ecx_dados    =   sys.exc_info()
+            ecx_nome                        =   os.path.split(ecx_dados.tb_frame.f_code.co_filename)[1]
+
+            vtmp_erro   =   '[CORE][LISTA_CAMERAS][ERRO][' + ecx_dados.tb_lineno + '] - ' + p_erro
+            self.proc_salva_arquivo(vtmp_erro)
+
+            return None
+    # Coleta de dados da câmera e execução dos procedimentos necessários
+
+    # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- #
+
+    # executa as imagens enquanto existir informação - MULTIPROCESSING
+    def exec_visao(self):
+        try:
+            # Coleta a lista de câmeras cadastradas para tratamento
+            vtmp_camera     =   self.lista_cameras()
+            # Coleta a lista de câmeras cadastradas para tratamento
+
+            # Lista de dados para detecção - Câmeras disponíveis
+            if vtmp_camera is None or len(vtmp_camera) <= 0:
+                vlista_camera       =   [['WebCam',0,50,None]]
+            else:
+                vlista_camera   =   [[vtmp_camera[curreg][0], vtmp_camera[curreg][1], vtmp_camera[curreg][2],vtmp_camera[curreg][3]]  for curreg in range(len(vtmp_camera))]
+            # Lista de dados para detecção - Câmeras disponíveis
+
+
+            # Inicia a verificação da lista de câmeras para execução
+            self.MB_VISAO['MB_INSTANCIA']   =   visao_mbrauna(self.MB_BANCO_DADOS['MB_CONEXAO'],self.MB_CONFIGURACAO)
+            self.MB_VISAO['MB_CAMERA']      =   vlista_camera
+
+            try:
+                self.MB_VISAO['MB_INSTANCIA'].executa(vlista_camera)
+            except Exception as p_erro:
+                # Mais detalhes sobre o erro
+                ecx_tipo, ecx_obj, ecx_dados    =   sys.exc_info()
+                ecx_nome                        =   os.path.split(ecx_dados.tb_frame.f_code.co_filename)[1]
+
+                vtmp_erro   =   '[CORE][EXEC_VISAO|VISAO][ERRO][' + ecx_dados.tb_lineno + '] - ' + p_erro
+                self.proc_salva_arquivo(vtmp_erro)
+
+        except Exception as p_erro:
+            # Mais detalhes sobre o erro
+            ecx_tipo, ecx_obj, ecx_dados    =   sys.exc_info()
+            ecx_nome                        =   os.path.split(ecx_dados.tb_frame.f_code.co_filename)[1]
+
+            vtmp_erro   =   '[CORE][EXEC_VISAO][ERRO][' + ecx_dados.tb_lineno + '] - ' + p_erro
+            self.proc_salva_arquivo(vtmp_erro)
+    # executa as imagens enquanto existir informação - MULTIPROCESSING
+
+    # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- #
+
+    # ---------------------------------------------------------------- #
+    #                 PROCEDIMENTOS PRINCIPAIS AO CORE                 #
+    # ---------------------------------------------------------------- #
