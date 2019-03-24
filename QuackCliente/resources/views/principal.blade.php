f152b6a44fe52179cffa83908871277077a734f9
Michel Brauna <michelbrauna@gmail.com>
2019-03-24 11:31:17 -0300

    CRUD - Performance e execução
    Melhoria nos procedimentos de inserção e consulta.
    A forma de execução foi alterada, sempre ao executar a Thred irá validar permissões externas.

--- QuackCliente/resources/views/principal.blade.php
+++ QuackCliente/resources/views/principal.blade.php
@@ -24,6 +24,7 @@
 @section('conteudo_principal')
 <section class="content content--layout">
     <h2 class="word word--playful">Quack</h2>
+    <p class="center">Pensou novo? Quack</p>
 </section>
 @endsection
 
@@ -39,153 +40,63 @@
 
 
 @section('conteudo')
-<div class="card">
-    <div class="card-header">
-      <ul class="nav nav-tabs nav-tabs-primary" role="tablist">
-        <li class="nav-item">
-          <a class="nav-link active" data-toggle="tab" href="#link1" role="tablist">
-            <i class="tim-icons icon-spaceship"></i> Chatbot
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" data-toggle="tab" href="#link2" role="tablist">
-            <i class="tim-icons icon-settings-gear-63"></i> Detecção
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" data-toggle="tab" href="#link3" role="tablist">
-            <i class="tim-icons icon-bag-16"></i> Predição
-          </a>
-        </li>
-      </ul>
-    </div>
-    <div class="card-body">
-      <!-- Tab panes -->
-      <div class="tab-content tab-space">
-        <div class="tab-pane active" id="link1">
-            <p>blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá</p>
-            <p>blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá</p>
-            <p>blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá</p>
-        </div>
-        <div class="tab-pane" id="link2">
-          <p>blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá</p>
-          <p>blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá</p>
-          <p>blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá</p>
-          <p>blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá</p>
-          <p>blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá</p>
-          <p>blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá</p>
-          <p>blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá</p>
-          <p>blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá</p>
-          <p>blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá</p>
-        </div>
-        <div class="tab-pane" id="link3">
-          <p>blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá</p>
-          <p>blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá</p>
-          <p>blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá</p>
-          <p>blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá</p>
-          <p>blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá</p>
-          <p>blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá</p>
-        </div>
-      </div>
-    </div>
-  </div>
-
-
 <div class="section section-tabs">
     <div class="container">
-        <div class="title">
-            <h3 class="mb-3">Serviços</h3>
-        </div>
         <div class="row">
             <div class="col-md-10 ml-auto col-xl-10 mr-auto">
-              <!-- Nav tabs -->
-              <div class="card">
-                <div class="card-header">
-                  <ul class="nav nav-tabs nav-tabs-primary" role="tablist">
-                    <li class="nav-item">
-                      <a class="nav-link active" data-toggle="tab" href="#link1" role="tablist">
-                        <i class="tim-icons icon-spaceship"></i> Profile
-                      </a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link" data-toggle="tab" href="#link2" role="tablist">
-                        <i class="tim-icons icon-settings-gear-63"></i> Settings
-                      </a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link" data-toggle="tab" href="#link3" role="tablist">
-                        <i class="tim-icons icon-bag-16"></i> Options
-                      </a>
-                    </li>
-                  </ul>
+                <div class="card">
+                    <div class="card-header">
+                        <ul class="nav nav-tabs nav-tabs-primary" role="tablist">
+                            <li class="nav-item">
+                                <a class="nav-link active" data-toggle="tab" href="#link1" role="tablist">
+                                    <i class="tim-icons icon-spaceship"></i> Detecção
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" data-toggle="tab" href="#link2" role="tablist">
+                                    <i class="tim-icons icon-settings-gear-63"></i> Predição
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" data-toggle="tab" href="#link3" role="tablist">
+                                    <i class="tim-icons icon-bag-16"></i> Options
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="card-body">
+                        <div class="tab-content tab-space">
+                            <div class="tab-pane active" id="link1">
+                                <p>
+                                    Collaboratively administrate empowered markets via plug-and-play networks. Dynamically   procrastinate B2C users after installed base benefits.
+                                    <br />
+                                    <br/>
+                                    Dramatically visualize customer directed convergence without revolutionary ROI.
+                                </p>
+                            </div>
+                            <div class="tab-pane" id="link2">
+                                <p>
+                                    Completely synergize resource taxing relationships via premier niche markets. Professionally cultivate one-to-one customer service with robust ideas.
+                                    <br />
+                                    <br/>
+                                    Dynamically innovate resource-leveling customer service for state of the art customer service.
+                                </p>
+                            </div>
+                            <div class="tab-pane" id="link3">
+                                <p>
+                                    Efficiently unleash cross-media information without cross-media value. Quickly maximize timely deliverables for real-time schemas.
+                                    <br />
+                                    <br/>
+                                    Dramatically maintain clicks-and-mortar solutions without functional solutions.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="card-body">
-                  <!-- Tab panes -->
-                  <div class="tab-content tab-space">
-                    <div class="tab-pane active" id="link1">
-                      <p>Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits.
-                        <br />
-                        <br/> Dramatically visualize customer directed convergence without revolutionary ROI.</p>
-                    </div>
-                    <div class="tab-pane" id="link2">
-                      <p>Completely synergize resource taxing relationships via premier niche markets. Professionally cultivate one-to-one customer service with robust ideas.
-                        <br />
-                        <br/>Dynamically innovate resource-leveling customer service for state of the art customer service.</p>
-                    </div>
-                    <div class="tab-pane" id="link3">
-                      <p>Efficiently unleash cross-media information without cross-media value. Quickly maximize timely deliverables for real-time schemas.
-                        <br />
-                        <br/> Dramatically maintain clicks-and-mortar solutions without functional solutions.</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
             </div>
-            <div class="col-md-10 ml-auto col-xl-6 mr-auto">
-              <div class="mb-3">
-                <small class="text-uppercase font-weight-bold">With text</small>
-              </div>
-              <!-- Tabs with Background on Card -->
-              <div class="card">
-                <div class="card-header">
-                  <ul class="nav nav-tabs nav-tabs-primary" role="tablist">
-                    <li class="nav-item">
-                      <a class="nav-link active" data-toggle="tab" href="#link4" role="tablist">
-                        Profile
-                      </a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link" data-toggle="tab" href="#link5" role="tablist">
-                        Settings
-                      </a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link" data-toggle="tab" href="#link6" role="tablist">
-                        Options
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-                <div class="card-body">
-                  <!-- Tab panes -->
-                  <div class="tab-content tab-space">
-                    <div class="tab-pane active" id="link4">
-                      <p> These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided.
-                        <br/>But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures</p>
-                    </div>
-                    <div class="tab-pane" id="link5">
-                      <p> I will be the leader of a company that ends up being worth billions of dollars, because I got the answers. I understand culture. I am the nucleus. I think that’s a responsibility that I have, to push possibilities, to show people, this is the level that things could be at. I think that’s a responsibility that I have, to push possibilities, to show people, this is the level that things could be at. </p>
-                    </div>
-                    <div class="tab-pane" id="link6">
-                      <p>I think that’s a responsibility that I have, to push possibilities, to show people, this is the level that things could be at. So when you get something that has the name Kanye West on it, it’s supposed to be pushing the furthest possibilities. I will be the leader of a company that ends up being worth billions of dollars, because I got the answers. I understand culture. I am the nucleus.</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <!-- End Tabs on plain Card -->
-            </div>
-          </div>
         </div>
+    </div>
+</div>
 @endsection
 
 
@@ -196,20 +107,7 @@
 
 
 @section('scripts')
-<svg class="hidden">
-    <symbol id="icon-arrow" viewBox="0 0 24 24">
-        <title>arrow</title>
-        <polygon points="6.3,12.8 20.9,12.8 20.9,11.2 6.3,11.2 10.2,7.2 9,6 3.1,12 9,18 10.2,16.8 "/>
-    </symbol>
-    <symbol id="icon-drop" viewBox="0 0 24 24">
-        <title>drop</title>
-        <path d="M12,21c-3.6,0-6.6-3-6.6-6.6C5.4,11,10.8,4,11.4,3.2C11.6,3.1,11.8,3,12,3s0.4,0.1,0.6,0.3c0.6,0.8,6.1,7.8,6.1,11.2C18.6,18.1,15.6,21,12,21zM12,4.8c-1.8,2.4-5.2,7.4-5.2,9.6c0,2.9,2.3,5.2,5.2,5.2s5.2-2.3,5.2-5.2C17.2,12.2,13.8,7.3,12,4.8z"/><path d="M12,18.2c-0.4,0-0.7-0.3-0.7-0.7s0.3-0.7,0.7-0.7c1.3,0,2.4-1.1,2.4-2.4c0-0.4,0.3-0.7,0.7-0.7c0.4,0,0.7,0.3,0.7,0.7C15.8,16.5,14.1,18.2,12,18.2z"/>
-    </symbol>
-    <symbol id="icon-github" viewBox="0 0 32.6 31.8">
-        <title>github</title>
-        <path d="M16.3,0C7.3,0,0,7.3,0,16.3c0,7.2,4.7,13.3,11.1,15.5c0.8,0.1,1.1-0.4,1.1-0.8c0-0.4,0-1.4,0-2.8c-4.5,1-5.5-2.2-5.5-2.2c-0.7-1.9-1.8-2.4-1.8-2.4c-1.5-1,0.1-1,0.1-1c1.6,0.1,2.5,1.7,2.5,1.7c1.5,2.5,3.8,1.8,4.7,1.4c0.1-1.1,0.6-1.8,1-2.2c-3.6-0.4-7.4-1.8-7.4-8.1c0-1.8,0.6-3.2,1.7-4.4C7.4,10.7,6.8,9,7.7,6.8c0,0,1.4-0.4,4.5,1.7c1.3-0.4,2.7-0.5,4.1-0.5c1.4,0,2.8,0.2,4.1,0.5c3.1-2.1,4.5-1.7,4.5-1.7c0.9,2.2,0.3,3.9,0.2,4.3c1,1.1,1.7,2.6,1.7,4.4c0,6.3-3.8,7.6-7.4,8c0.6,0.5,1.1,1.5,1.1,3c0,2.2,0,3.9,0,4.5c0,0.4,0.3,0.9,1.1,0.8c6.5-2.2,11.1-8.3,11.1-15.5C32.6,7.3,25.3,0,16.3,0z"/>
-    </symbol>
-</svg>
+
 <!-- JavaScript Core -->
 <script src="./Quack/js/core/jquery.min.js"                         type="text/javascript"></script>
 <script src="./Quack/js/core/popper.min.js"                         type="text/javascript"></script>
